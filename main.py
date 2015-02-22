c126ac0c568ea370b485adc598d556fbb49e287c
Bas Vb <bas.berkel@student.ru.nl>
2015-02-22 22:32:53 +0100

    Remove unused imports

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ __author__ = ''
 
 #Import libraries for doing image analysis
 from skimage.io import imread
-from skimage.transform import resize
 from sklearn.ensemble import RandomForestClassifier as RF
 import glob
 import os
@@ -10,16 +9,12 @@ from sklearn import cross_validation
 from sklearn.cross_validation import StratifiedKFold as KFold
 from sklearn.metrics import classification_report
 from matplotlib import pyplot as plt
-from matplotlib import colors
 from pylab import cm
-from skimage import segmentation
-from skimage.morphology import watershed
 from skimage import measure
 from skimage import morphology
 import numpy as np
 import pandas as pd
-from scipy import ndimage
-from skimage.feature import peak_local_max
+
 
 #imports van eigen classes
 import multiclass_log_loss
